ed596abc8d23e61db854fb6081c456021f8fb2af
CO17349 <ripjeet7777@gmail.com>
2023-02-06 13:38:09 -0600

    Grid pane multiple rows

--- src/JavaBasics/GridPaneDemo.java
+++ src/JavaBasics/GridPaneDemo.java
@@ -1,6 +1,7 @@
 package JavaBasics;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -24,8 +25,13 @@ public class GridPaneDemo extends Application {
         pane.add(new TextField(), 1, 1);
         pane.add(new Label("Last Name:"), 0, 2);
         pane.add(new TextField(), 1, 2);
+        pane.addRow(3, new Label("City:"), new TextField(), new Label("Prov:"), new TextField());
+
         Button btnAdd = new Button("Add Name");
-        pane.add(btnAdd, 1, 3);
+        pane.add(btnAdd, 1, 4);
+
+
+        pane.setHalignment(btnAdd, HPos.RIGHT);
 
         Scene scene = new Scene(pane, 500, 300);
         stage.setTitle("GridPane Demo");
